aa46fa34c92d30244413afabed543a418790f8dc
UnrelatedString <unrelatedstring@gmail.com>
2025-05-05 11:29:35 -0400

    first things first just do the bigints thing AND WHAT THE FUCK WHY ARE INTS RESTRICTED TO 32 BITS WHEN SAFE INTS GO OUT TO LIKE 53

--- src/Data/Snowflake.purs
+++ src/Data/Snowflake.purs
@@ -4,12 +4,6 @@ module Data.Snowflake
   , fromBase10
   , toTimestamp
   , fromTimestamp
-  , SnowflakeTimestamp
-  , discordEpoch
-  , toUnix
-  , fromUnix
-  , toInstant
-  , fromInstant
   , StringSnowflake
   ) where
 
@@ -26,7 +20,10 @@ import Data.String.CodePoints (toCodePointArray, codePointFromChar, fromCodePoin
 import Data.String.CodePoints as String
 import Data.Time.Duration (Milliseconds(..))
 import Data.Traversable (traverse)
+import JS.BigInt as BigInt -- just change this to a second Data.Int import for Purerl :p
 import Partial.Unsafe (unsafePartial)
+
+import Data.Snowflake.Timestamp (Timestamp(..))
 
 -- | A class for types which represent Discord "snowflake" IDs,
 -- | which are inherently 64-bit integers and can be accessed as such if
@@ -38,64 +35,17 @@ import Partial.Unsafe (unsafePartial)
 class Snowflake a where
   toBase10 :: a -> String
   fromBase10 :: String -> Maybe a
-  toTimestamp :: a -> SnowflakeTimestamp
+  toTimestamp :: a -> Timestamp
   -- | Should have non-timestamp bits zeroed.
-  fromTimestamp :: SnowflakeTimestamp -> a
+  fromTimestamp :: Timestamp -> a
 
--- | Milliseconds since the Discord Epoch, as a 42-bit unsigned integer.
--- | (This is well within the safe limits for doubles.)
-newtype SnowflakeTimestamp = SnowflakeTimestamp Int
-
-derive newtype instance eqTimestamp :: Eq SnowflakeTimestamp
-derive newtype instance ordTimestamp :: Ord SnowflakeTimestamp
-derive instance showTimestamp :: Show SnowflakeTimestamp
-
-instance Bounded SnowflakeTimestamp where
-  bottom = SnowflakeTimestamp 0
-  top = SnowflakeTimestamp 0x3ffffffffff
-
-instance Enum SnowflakeTimestamp where
-  pred t
-    | t == bottom = Nothing
-  pred (SnowflakeTimestamp i) = Just $ SnowflakeTimestamp $ i - 1
-
-  succ t
-    | t == top = Nothing
-  succ (SnowflakeTimestamp i) = Just $ SnowflakeTimestamp $ i + 1
-
-instance BoundedEnum SnowflakeTimestamp where
-  cardinality = Cardinality 0x40000000000
-  toEnum i
-    i < 0 = Nothing
-    i > 0x3ffffffffff = Nothing
-    otherwise = Just $ SnowflakeTimestamp i
-  fromEnum (SnowflakeTimestamp i) = i
-
--- | 1420070400000;
--- | the number of milliseconds from the Unix epoch to
--- | the first second of 2015.
-discordEpoch :: Int
-discordEpoch = 1420070400000
-
-toUnix :: SnowflakeTimestamp -> Milliseconds
-toUnix (SnowflakeTimestamp i) = Milliseconds $ toNumber $ i + discordEpoch
-
-fromUnix :: Milliseconds -> Maybe SnowflakeTimestamp
-fromUnix (Milliseconds n) = toEnum $ ceil n - discordEpoch
-
-toInstant :: SnowflakeTimestamp -> Maybe Instant.Instant
-toInstant = Instant.instant <<< toUnix
-
-fromInstant :: Instant.Instant -> Maybe SnowflakeTimestamp
-fromInstant = fromUnix <<< Instant.uninstant
-
+shift :: BigInt.BigInt
+shift = BigInt.fromInt 22
 
 -- | A universal string representation of snowflakes,
--- | for use by JSON-based runners.
--- | Note that while the constructor is intentionally not exposed,
--- | the main way of obtaining `StringSnowflake`s is intended
--- | to be through foreign declarations or `unsafeCoerce`
--- | from unvalidated trusted JSON payloads.
+-- | for use with API endpoints which serve or receive JSON
+-- | and natively use strings as is because 64 bit integers
+-- | are outside the safe integer limit for doubles (as used by JS).
 
 newtype StringSnowflake = StringSnowflake String
 
@@ -116,10 +66,17 @@ instance Snowflake StringSnowflake where
       = Nothing
     | otherwise = Just s
 
-  -- could just do this with bigints... but 1. portability and 2. sunk cost :p
+  -- | Will error at runtime if given a malformed snowflake!
+  -- | Only construct snowflakes from trusted foreign data or
+  -- | through `fromBase10`'s validation.
   toTimestamp (StringSnowflake s) = unsafePartial $ fromJust do
-    digits <- traverse (fromString <<< String.singleton) $ toCodePointArray s
-    -- max 20 digits if well-formed, so it can't ever over-divide
-    let safe = baseFiveHalves $ fromEnum <$> digits
-
-    where baseFiveHalves :: 
+    value <- BigInt.fromString s
+    let timestampBits = BigInt.shr value shift
+    timestampInt <- BigInt.toInt timestampBits
+    pure $ Timestamp timestampBits
+  
+  fromTimestamp (Timestamp t)
+    = StringSnowflake
+    $ BigInt.toString
+    $ flip BigInt.shl shift
+    $ BigInt.fromInt t
--- src/Data/Snowflake/Timestamp.purs
+++ src/Data/Snowflake/Timestamp.purs
@@ -1,11 +1,10 @@
 module Data.Snowflake.Timestamp
-  ( SnowflakeTimestamp
+  ( Timestamp(..)
   , discordEpoch
   , toUnix
   , fromUnix
   , toInstant
   , fromInstant
-  , StringSnowflake
   ) where
 
 import Prelude
@@ -14,57 +13,48 @@ import Data.Array as Array
 import Data.DateTime.Instant as Instant
 import Data.Enum (class Enum, class BoundedEnum, Cardinality(..), toEnum, fromEnum)
 import Data.Foldable (any)
-import Data.FoldableWithIndex (class FoldableWithIndex, foldrWithIndex)
+import Data.Generic.Rep (class Generic)
 import Data.Int (toNumber, ceil, fromString, pow)
 import Data.Maybe (Maybe(..), fromJust)
+import Data.Show.Generic (genericShow)
 import Data.String.CodePoints (toCodePointArray, codePointFromChar, fromCodePointArray)
 import Data.String.CodePoints as String
 import Data.Time.Duration (Milliseconds(..))
 import Data.Traversable (traverse)
 import Partial.Unsafe (unsafePartial)
 
--- | A class for types which represent Discord "snowflake" IDs,
--- | which are inherently 64-bit integers and can be accessed as such if
--- | interfacing with the API through Erlang's External Term Format
--- | but are provided serialized as base 10 strings
--- | if interfacting with the API through JSON
--- | and are additionally used in base 10 representation in messages.
+-- | Timestamps contained by snowflakes,
+-- | which can be inspected to determine the time of the referenced entity's creation,
+-- | or created to invent ad-hoc snowflakes for things like Discord API pagination.
+newtype Timestamp = Timestamp Int
 
-class Snowflake a where
-  toBase10 :: a -> String
-  fromBase10 :: String -> Maybe a
-  toTimestamp :: a -> SnowflakeTimestamp
-  -- | Should have non-timestamp bits zeroed.
-  fromTimestamp :: SnowflakeTimestamp -> a
+derive newtype instance eqTimestamp :: Eq Timestamp
+derive newtype instance ordTimestamp :: Ord Timestamp
+derive instance genericTimestamp :: Generic Timestamp _
 
--- | Milliseconds since the Discord Epoch, as a 42-bit unsigned integer.
--- | (This is well within the safe limits for doubles.)
-newtype SnowflakeTimestamp = SnowflakeTimestamp Int
+instance showTimestamp :: Show Timestamp where
+  show = genericShow
 
-derive newtype instance eqTimestamp :: Eq SnowflakeTimestamp
-derive newtype instance ordTimestamp :: Ord SnowflakeTimestamp
-derive instance showTimestamp :: Show SnowflakeTimestamp
+instance boundedTimestamp :: Bounded Timestamp where
+  bottom = Timestamp 0
+  top = Timestamp 0x3ffffffffff
 
-instance Bounded SnowflakeTimestamp where
-  bottom = SnowflakeTimestamp 0
-  top = SnowflakeTimestamp 0x3ffffffffff
-
-instance Enum SnowflakeTimestamp where
+instance enumTimestamp :: Enum Timestamp where
   pred t
     | t == bottom = Nothing
-  pred (SnowflakeTimestamp i) = Just $ SnowflakeTimestamp $ i - 1
+  pred (Timestamp i) = Just $ Timestamp $ i - 1
 
   succ t
     | t == top = Nothing
-  succ (SnowflakeTimestamp i) = Just $ SnowflakeTimestamp $ i + 1
+  succ (Timestamp i) = Just $ Timestamp $ i + 1
 
-instance BoundedEnum SnowflakeTimestamp where
+instance boundedEnumTimestamp :: BoundedEnum Timestamp where
   cardinality = Cardinality 0x40000000000
   toEnum i
-    i < 0 = Nothing
-    i > 0x3ffffffffff = Nothing
-    otherwise = Just $ SnowflakeTimestamp i
-  fromEnum (SnowflakeTimestamp i) = i
+    | i < 0 = Nothing
+    | i > 0x3ffffffffff = Nothing
+    | otherwise = Just $ Timestamp i
+  fromEnum (Timestamp i) = i
 
 -- | 1420070400000;
 -- | the number of milliseconds from the Unix epoch to
@@ -72,49 +62,14 @@ instance BoundedEnum SnowflakeTimestamp where
 discordEpoch :: Int
 discordEpoch = 1420070400000
 
-toUnix :: SnowflakeTimestamp -> Milliseconds
-toUnix (SnowflakeTimestamp i) = Milliseconds $ toNumber $ i + discordEpoch
+toUnix :: Timestamp -> Milliseconds
+toUnix (Timestamp i) = Milliseconds $ toNumber $ i + discordEpoch
 
-fromUnix :: Milliseconds -> Maybe SnowflakeTimestamp
+fromUnix :: Milliseconds -> Maybe Timestamp
 fromUnix (Milliseconds n) = toEnum $ ceil n - discordEpoch
 
-toInstant :: SnowflakeTimestamp -> Maybe Instant.Instant
+toInstant :: Timestamp -> Maybe Instant.Instant
 toInstant = Instant.instant <<< toUnix
 
-fromInstant :: Instant.Instant -> Maybe SnowflakeTimestamp
-fromInstant = fromUnix <<< Instant.uninstant
-
-
--- | A universal string representation of snowflakes,
--- | for use by JSON-based runners.
--- | Note that while the constructor is intentionally not exposed,
--- | the main way of obtaining `StringSnowflake`s is intended
--- | to be through foreign declarations or `unsafeCoerce`
--- | from unvalidated trusted JSON payloads.
-
-newtype StringSnowflake = StringSnowflake String
-
-derive newtype instance stringSnowflakeEq :: Eq StringSnowflake
-derive newtype instance stringSnowflakeOrd :: Ord StringSnowflake
-derive instance stringSnowflakeShow :: Show StringSnowflake
-
-instance Snowflake StringSnowflake where
-  toBase10 (StringSnowflake s) = s
-
-  -- ...reminds me I kinda want to actually make that safe regex library now
-  fromBase10 s
-    | any
-      (\c -> c < codePointFromChar '0' || c > codePointFromChar '9')
-      (toCodePointArray s)
-      = Nothing
-    | fromCodePointArray (Array.reverse $ toCodePointArray s) >= "61615590737044764481"
-      = Nothing
-    | otherwise = Just s
-
-  -- could just do this with bigints... but 1. portability and 2. sunk cost :p
-  toTimestamp (StringSnowflake s) = unsafePartial $ fromJust do
-    digits <- traverse (fromString <<< String.singleton) $ toCodePointArray s
-    -- max 20 digits if well-formed, so it can't ever over-divide
-    let safe = baseFiveHalves $ fromEnum <$> digits
-
-    where baseFiveHalves :: 
+fromInstant :: Instant.Instant -> Maybe Timestamp
+fromInstant = fromUnix <<< Instant.unInstant
